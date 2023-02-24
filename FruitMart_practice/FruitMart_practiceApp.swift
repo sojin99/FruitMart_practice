6745c8a48cbcb7cbda58de29318b4910e0ce0dd5
sojin kwon <thwls4597@naver.com>
2023-02-24 15:56:11 +0900

    Initial Commit

---
+++ FruitMart_practice/FruitMart_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMart_practiceApp.swift
+//  FruitMart_practice
+//
+//  Created by 권소진 on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitMart_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
